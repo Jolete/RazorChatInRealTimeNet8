b2c69d5b4b1d035838196bfd86434f6e3850fcc4
jordi.arenas <jordi.arenas@quantion.com>
2024-07-23 03:37:53 +0200

    Agregar archivos de proyecto.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RazorChatInRealTimeNet8.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
